90976ed52838e1a8eab57460b54712d8ab6ff7a1
bruno-sales <brunosales-rj@hotmail.com>
2016-05-29 23:57:30 -0300

    Utilizando cookies
    recuperar nome do usuario logado

--- src/java/Controller/GogoServlet.java
+++ src/java/Controller/GogoServlet.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -38,9 +39,8 @@ public class GogoServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        try (PrintWriter out = response.getWriter()) 
-        {
+
+        try (PrintWriter out = response.getWriter()) {
             String tarefa = request.getParameter("t");
 
             switch (tarefa) {
@@ -72,75 +72,76 @@ public class GogoServlet extends HttpServlet {
         //se algum nulo, retorna erro
         if (login == null || senha == null) {
             response.sendRedirect("login.jsp?mensagem=*Login ou senha invalido!");
-        }else
-        {
+        } else {
 
-        UsuarioDAO userDao = new UsuarioDAO();
-        //Metodo interno de verificação de Login
-        Usuario usuario = userDao.verificaLogin(login, senha);
+            UsuarioDAO userDao = new UsuarioDAO();
+            //Metodo interno de verificação de Login
+            Usuario usuario = userDao.verificaLogin(login, senha);
 
-        if (usuario == null) {
-            response.sendRedirect("login.jsp?mensagem=*Login ou senha invalido!");
-        }else
-        {
-        request.setAttribute("usuario", usuario);
-        
-        // Redireciona
-        RequestDispatcher rd = request.getRequestDispatcher("/listaPersonagens.jsp");
-        rd.forward(request, response);
-        }
+            if (usuario == null) {
+                response.sendRedirect("login.jsp?mensagem=*Login ou senha invalido!");
+            } else {
+
+                Cookie loginCookie = new Cookie("user", usuario.getNome());
+                //cookie expirando em 30 mins                
+                loginCookie.setMaxAge(30 * 60);
+
+                //cria o cookie
+                response.addCookie(loginCookie);
+
+                //redireciona
+                response.sendRedirect("listaPersonagens.jsp");
+            }
         }
     }
 
     public void cadastrarUsuario(HttpServletRequest request,
             HttpServletResponse response) throws IOException, ServletException {
-        
+
         UsuarioDAO uDao = new UsuarioDAO();
 
         Usuario novoUser = new Usuario();
-        
+
         String nome = request.getParameter("nome");
         String cpf = request.getParameter("cpf");
         String email = request.getParameter("email");
         String telefone = request.getParameter("telefone");
         String senha = request.getParameter("senha");
-        
+
         novoUser.setEmail(email);
         novoUser.setNome(nome);
         novoUser.setCpf(cpf);
         novoUser.setTelefone(telefone);
         novoUser.setSenha(senha);
-        
+
         boolean retorno = uDao.inserir(novoUser);
 
-         if (retorno == false) {
+        if (retorno == false) {
             response.sendRedirect("cadastroUsuario.jsp?mensagem=Ocorreu um erro! Tente novamente");
-        }else{
-             response.sendRedirect("login.jsp");
-         }
-        
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+
     }
-    
+
     protected void buscarPersonagens(HttpServletRequest request,
-            HttpServletResponse response) throws IOException, ServletException 
-    {
+            HttpServletResponse response) throws IOException, ServletException {
         PersonagemDAO pDao = new PersonagemDAO();
 
-        List<Personagem> lista = new ArrayList<>();
-        
-        lista = pDao.lista();
-                
+        List<Personagem> lista = pDao.lista();
+
         request.setAttribute("personas", lista);
-                
+
         // Redireciona
         RequestDispatcher rd = request.getRequestDispatcher("/listaPersonagens.jsp");
         rd.forward(request, response);
-        
+
     }
-         
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -148,12 +149,13 @@ public class GogoServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -161,18 +163,18 @@ public class GogoServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
         return "Servle da aplicacao";
     }// </editor-fold>
-
 
 }
