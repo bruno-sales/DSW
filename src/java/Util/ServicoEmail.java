462cd0c6a5b307efec469d500e80afd8b852f79c
bruno-sales <brunosales-rj@hotmail.com>
2016-06-03 03:38:12 -0300

    Mail Service
    Para implementação dos tokens e recuperação de senha

---
+++ src/java/Util/ServicoEmail.java
@@ -0,0 +1,72 @@
+package Util;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+/**
+ *
+ * Classe responsavel pelo envio de emails
+ */
+public class ServicoEmail {
+
+    private final String sender = "bolsagogodsw@gmail.com";
+    private final String pass = "servidorweb";
+
+    private final String host = "smtp.gmail.com";
+    private final String socketPort = "465";
+    private final String socketClass = "javax.net.ssl.SSLSocketFactory";
+    private final String port = "465";
+    private final Properties props;
+
+    public ServicoEmail() {
+        props = new Properties();
+        
+        /**
+         * Parâmetros de conexão com servidor Gmail
+         */
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.socketFactory.port", socketPort);
+        props.put("mail.smtp.socketFactory.class", socketClass);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", port);
+
+    }
+
+    public void EnviarEmail(String destinatario, String titulo, String mensagem) {
+
+        try {
+
+            // Cria sessão para o email
+            Session sessionEmail = Session.getInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(sender, pass);
+                        }
+                    });
+
+            // Para quem o email se destina.
+            String to = destinatario;
+
+            // Cria MimeMessage.
+            MimeMessage message = new MimeMessage(sessionEmail);
+
+            // Cabeçalho: Quem enviou.
+            message.setFrom(new InternetAddress(sender));
+
+            // Cabeçalho: Quem receberá.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Cabeçalho: Titulo
+            message.setSubject(titulo);
+
+            // Corpo do email
+            message.setText(mensagem);
+
+            // Transportar mensagem, enviar.
+            Transport.send(message);
+            
+        } catch (MessagingException mex) {
+        }
+    }
+}
